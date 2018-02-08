899819607d9b55561da29e8f2d1e47e924b90b89
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2018-02-08 21:14:10 +0100

    completes merge

--- znc/src/startup-sequence/01-options.sh
+++ znc/src/startup-sequence/01-options.sh
@@ -1,3 +1,3 @@
 # Might be overriden in some later file, when using "FROM znc"
 
-DATADIR="/znc-data"
+DATADIR="/data"
--- znc/src/startup-sequence/90-config.sh
+++ znc/src/startup-sequence/90-config.sh
@@ -1,3 +1,2 @@
 mkdir -p /data/configs
 cp -n /znc.conf /data/configs/znc.conf
-DATADIR="/data"
